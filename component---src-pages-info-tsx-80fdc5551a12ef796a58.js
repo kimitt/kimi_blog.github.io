c2e6743c6cf1b439d0e551b55a90abcedfbd884a
kimi <ms.kim@danielproject.co.kr>
2022-07-21 18:01:07 +0900

    Updates

---
+++ component---src-pages-info-tsx-80fdc5551a12ef796a58.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[977],{8187:function(t,a,e){e.r(a);var r=e(3431);a.default=function(t){var a=t.data.site.siteMetadata,e=a.title,s=a.description,u=a.author;return(0,r.tZ)("div",null,e," ",s," ",u)}}}]);
+//# sourceMappingURL=component---src-pages-info-tsx-80fdc5551a12ef796a58.js.map
